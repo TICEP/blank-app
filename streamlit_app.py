7f1c618365217cd235d10b76797211d4527f989b
TICEP <TICEP@twyford.org.uk>
2025-12-27 16:12:56 +0000

    Encryption app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ st.title("🎈 My new Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+key = int(input("Enter the key"))
+message = input("Enter the message")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+new_message = ""
+for character in message:
+    if character in alphabet:
+        position = alphabet.find(character)
+        new_position = (position + key)
+        new_character = alphabet[new_position]
+        new_message += new_character
+print(new_message)
